8c031b282a911dd6ceaa292cd292e1f6c38c308e
Kartik Sharma <kartiksrm06@gmail.com>
2026-07-13 13:29:10 +0530

    Time: 1 ms (79.04%), Space: 46.6 MB (81.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -7,7 +7,7 @@ class Solution {
 		int k = 0 ;
 		for (int i=0; i<arr.length; i++) {
 			if(i<arr.length - 1 && arr[i]==arr[i+1]) {
-				continue;
+				// continue;
 			}
 			else {
 				arr[k] = arr[i];
