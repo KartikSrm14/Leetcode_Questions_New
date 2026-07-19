3d874c09bcef1c27c3be194c6ccea769fa9f37ea
Kartik Sharma <kartiksrm06@gmail.com>
2026-07-19 17:03:00 +0530

    Time: 6 ms (50.9%), Space: 56 MB (6.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        return Moore_Voting(nums);
+        return zoey(nums);
     }
-    public static int Moore_Voting(int[] arr) {
-		int e = arr[0];
-		int vote = 1;
-		for (int i=0; i<arr.length; i++) {
-			if(arr[i]==e) {
-				vote++;
-			}
-			else {
-				vote--;
-				if (vote==0) {
-					e = arr[i];
-					vote =1;
-				}
-			}
-		}
-		return e;
-	}
+
+    public static int zoey(int[] arr){
+        Arrays.sort(arr);
+        return arr[arr.length/2];
+    }
 }
