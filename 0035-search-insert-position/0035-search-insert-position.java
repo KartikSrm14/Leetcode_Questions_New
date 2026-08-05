459d583e065154d5e74f773db60168d3eac73367
Kartik Sharma <kartiksrm06@gmail.com>
2026-08-05 23:23:31 +0530

    Time: 0 ms (100%), Space: 44.3 MB (98.24%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,14 +1,24 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return Insert(nums, target);
-        
+        return zoo(nums,target);
     }
-    public static int Insert(int[] arr, int target) {
-		for (int i=0; i<arr.length; i++) {
-			if(arr[i] >= target) {
-				return i;
-			}
-		}
-		return arr.length;
-	}
+
+    public static int zoo(int[] arr,int x){
+        int lo = 0;
+        int hi = arr.length-1;
+        while(lo<=hi){
+            int mid =(lo+hi)/2;
+            if(arr[mid] == x){
+                return mid;
+            }
+            else if(arr[mid] > x){
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return lo;
+
+    }
 }
